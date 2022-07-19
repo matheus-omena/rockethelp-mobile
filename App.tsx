952ddaf04cc4a8835acba0479876588fcb61ac3b
Matheus Omena <omena.theus@live.com>
2022-07-19 18:28:12 -0300

    estrutural visual e telas da aplicação

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/styles/theme';
+
+import Loading from './src/components/Loading';
+import { Register } from './src/screens/Register';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      { fontsLoaded ? <Register /> : <Loading /> }
+    </NativeBaseProvider>
+  );
+}
